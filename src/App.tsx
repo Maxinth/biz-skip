bab024b319e59d592e706e27f57b17a6b0859f8c
maxinth <tunde.j.adeleye@gmail.com>
2025-04-01 21:18:19 +0100

    skipCard - init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <main className="text-3xl bg-red-500">Hello</main>;
+  return (
+    <main className="text-3xl bg-[#121212] h-screen">
+      <div className=" flex items-center justify-center flex-col w-full">
+        <h1 className="text-white text-2xl pt-12 mb-4">
+          Choose your skip size
+        </h1>
+        <p className="text-[#9ca3af] text-base">
+          Select the skip size that best suits your needs
+        </p>
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/SkipCard.tsx
@@ -0,0 +1,5 @@
+const SkipCard = () => {
+  return <div className="bg-[#1c1c1c]">SkipCard</div>;
+};
+
+export default SkipCard;
