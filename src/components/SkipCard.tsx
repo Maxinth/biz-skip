a4a93c952321c2f6f2dfaf03cb625182498285f1
maxinth <tunde.j.adeleye@gmail.com>
2025-04-02 08:16:15 +0100

    add updates to skipcard

--- src/components/SkipCard.tsx
+++ src/components/SkipCard.tsx
@@ -2,18 +2,25 @@ import SkipImg from "../assets/skip-img.png";
 
 const SkipCard = () => {
   return (
-    <div className="bg-[#1c1c1c] p-[26px] rounded-lg w-fit relative cursor-pointer">
-      <img src={SkipImg} alt="skip image item" />
-      <span className="absolute top-12 right-12 bg-[#0037c1] text-white text-sm font-medium rounded-full px-3 py-1">
+    <div className=" rounded-lg w-fit relative  cursor-pointer group  border-2 p-4 md:p-6 transition-all border-[#2A2A2A] hover:border-[#0037C1]/50 bg-[#1C1C1C] text-white ">
+      <img
+        src={SkipImg}
+        alt="4 yard skip"
+        className="w-full h-36 md:h-48 object-cover rounded-md mb-4"
+      />
+      <span className="absolute    px-3 py-1 top-8 right-8 z-20 bg-[#0037C1] text-white rounded-full text-sm font-medium shadow-md">
         4 Yards
       </span>
       <div className="mt-4 bg-[#1c1c1c]">
-        <p className="mb-2 text-white">4 Yard Skip</p>
-        <p className="text-[#9ca3af] mb-8"> 14 day hire period</p>
-        <p className="text-[#0037c1] text-xl font-medium mb-4">
-          £216 <span className="text-[#9ca3af] text-sm">per week</span>
+        <p className="mb-2 text-white  text-lg md:text-xl font-bold  ">
+          4 Yard Skip
         </p>
-        <button className="outline-none bg-[#3a3a3a] text-white text-base flex items-center justify-center w-full p-3 rounded-[6px] cursor-pointer">
+        <p className="text-[#9ca3af] mb-8 text-sm "> 14 day hire period</p>
+        <p className="text-[#0037c1] text-xl font-medium mb-4">
+          £216{" "}
+          <span className="text-[#9ca3af] text-sm font-normal">per week</span>
+        </p>
+        <button className="outline-none bg-[#2a2a2a] text-white text-base flex items-center justify-center w-full py-2.5 md:py-3 px-4 rounded-md transition-all  space-x-2 hover:bg-[#3A3A3A] hover:border-[#0037C1] p-3  cursor-pointer ">
           Select this skip
         </button>
       </div>
